a6d0640b71df18e9505909afa44b4f935c9a56b9
elsehow <ffff@ffffs-MacBook-Air.local>
2015-05-17 13:05:08 -0700

    fixed the bit that queries data between two times

--- getEntriesBetween.js
+++ getEntriesBetween.js
@@ -3,11 +3,9 @@ moment = require('moment')
 function getEntriesBetween (time1, time2, userId, sequelizeModel, successCb, errorCb) {
   sequelizeModel.findAll({
     where: {
-      indra_time: {
-        gt: moment(time1).format()
-      }
-      , indra_time: {
-      	lt: moment(time2).format()
+      createdAt: {
+          $lt: time2 
+        	, $gt: time1 
       }
       , id: userId
     }
